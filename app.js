502ce50ca819222d36f4d49f3fbe17285e3cb540
Ravikant singh <thakurravikantsingh550@gmail.com>
2026-06-28 22:22:38 +0530

    add user authantication and user schema

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings = require("./routes/listings.js");
 const reviews = require("./routes/reviews.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -50,11 +53,27 @@ app.get("/" , (req, res)=>{
 
 app.use(session(sessinOption));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req ,res , next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+
+app.get("/demouser" , async(req , res)=>{
+    let fakeUser = new User({
+        email:"student@gmail.com",
+        username:"delta-student",
+    });
+   let registerdUser = await User.register(fakeUser , "helloworld");
+   res.send(registerdUser);
 })
 
 app.use("/listings" , listings);
